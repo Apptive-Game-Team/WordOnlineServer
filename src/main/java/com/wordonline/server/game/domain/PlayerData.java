cccce5a5ffba8fb55db7c5a4ab82eb4db53b6ac3
Yunseong <dev.yunseong@gmail.com>
2025-05-06 20:59:34 +0900

    feature Player Data : Card 관련 함수 구현

--- src/main/java/com/wordonline/server/game/domain/PlayerData.java
+++ src/main/java/com/wordonline/server/game/domain/PlayerData.java
@@ -1,10 +1,27 @@
 package com.wordonline.server.game.domain;
 
-
 import java.util.List;
 
 public class PlayerData {
-    int mana;
-    int hp;
-    List<String> cards;
+    public final static int MAX_CARD_NUM = 6;
+
+    public int mana;
+    public int hp;
+    public List<String> cards;
+
+    // validate and add card
+    public boolean addCard(String card) {
+        if (MAX_CARD_NUM >= cards.size() + 1) {
+            cards.add(card);
+            return true;
+        }
+        return false;
+    }
+
+    // validate and use cards
+    public boolean useCards(String[] cards) {
+        // TODO validate using cards
+        return true;
+    }
+
 }
