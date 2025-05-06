81a21ed8895c26de9958adb2e14af3e26f9392c9
Yunseong <dev.yunseong@gmail.com>
2025-05-06 21:15:49 +0900

    feature PlayerData : max hp 설정, cards 초기화

--- src/main/java/com/wordonline/server/game/domain/PlayerData.java
+++ src/main/java/com/wordonline/server/game/domain/PlayerData.java
@@ -1,13 +1,15 @@
 package com.wordonline.server.game.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerData {
     public final static int MAX_CARD_NUM = 6;
+    public final static int MAX_HP = 100;
 
-    public int mana;
-    public int hp;
-    public List<String> cards;
+    public int mana = 0;
+    public int hp = MAX_HP;
+    public List<String> cards = new ArrayList<>();
 
     // validate and add card
     public boolean addCard(String card) {
@@ -23,5 +25,4 @@ public class PlayerData {
         // TODO validate using cards
         return true;
     }
-
 }
