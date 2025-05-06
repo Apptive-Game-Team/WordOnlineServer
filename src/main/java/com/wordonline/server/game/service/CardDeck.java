c2d9eb5b348896cd0ee710bdde465f20b8dfbdff
Yunseong <dev.yunseong@gmail.com>
2025-05-06 21:16:21 +0900

    feat CardDeck mutable 하게 수정, empty valid체크 추가

--- src/main/java/com/wordonline/server/game/service/CardDeck.java
+++ src/main/java/com/wordonline/server/game/service/CardDeck.java
@@ -4,16 +4,20 @@ import com.wordonline.server.game.domain.PlayerData;
 import com.wordonline.server.game.dto.CardInfoDto;
 import com.wordonline.server.game.dto.FrameInfoDto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class CardDeck {
     Random rand = new Random();
 
-    List<String> cards = List.of("Dummy", "Dummy", "Dummy", "Dummy", "Dummy", "Dummy");
+    List<String> cards = new ArrayList<>(List.of("Dummy", "Dummy", "Dummy", "Dummy", "Dummy", "Dummy"));
 
     // random pick card and update to Player Data, Frame Info Dto
     public void drawCard(PlayerData player, CardInfoDto cardInfoDto) {
+        if (cards.isEmpty())
+            return;
+
         int cardIndex = rand.nextInt(cards.size());
         String card = cards.get(cardIndex);
         if (player.addCard(card)){ // Added
