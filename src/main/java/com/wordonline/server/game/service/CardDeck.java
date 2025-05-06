e5ea1b09e1f7b4063263f7392b9fecb9402d7ec4
Yunseong <dev.yunseong@gmail.com>
2025-05-06 20:57:54 +0900

    feature Card Deck

---
+++ src/main/java/com/wordonline/server/game/service/CardDeck.java
@@ -0,0 +1,24 @@
+package com.wordonline.server.game.service;
+
+import com.wordonline.server.game.domain.PlayerData;
+import com.wordonline.server.game.dto.CardInfoDto;
+import com.wordonline.server.game.dto.FrameInfoDto;
+
+import java.util.List;
+import java.util.Random;
+
+public class CardDeck {
+    Random rand = new Random();
+
+    List<String> cards = List.of("Dummy", "Dummy", "Dummy", "Dummy", "Dummy", "Dummy");
+
+    // random pick card and update to Player Data, Frame Info Dto
+    public void drawCard(PlayerData player, CardInfoDto cardInfoDto) {
+        int cardIndex = rand.nextInt(cards.size());
+        String card = cards.get(cardIndex);
+        if (player.addCard(card)){ // Added
+            cardInfoDto.addCard(card);
+            cards.remove(cardIndex);
+        }
+    }
+}
