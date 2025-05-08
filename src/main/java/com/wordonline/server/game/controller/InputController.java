bf13195588aacc054e8c37c389a624ad952f810f
Yunseong <dev.yunseong@gmail.com>
2025-05-08 14:05:07 +0900

    feature inputController : 실제 GameLoop 로직이랑 연결;

--- src/main/java/com/wordonline/server/game/controller/InputController.java
+++ src/main/java/com/wordonline/server/game/controller/InputController.java
@@ -1,17 +1,29 @@
 package com.wordonline.server.game.controller;
 
+import com.wordonline.server.game.component.SessionManager;
 import com.wordonline.server.game.dto.InputRequestDto;
 import com.wordonline.server.game.dto.InputResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @MessageMapping("/game/input")
 public class InputController {
 
+    @Autowired
+    private SessionManager sessionManager;
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
     @MessageMapping("{sessionId}/{userId}")
-    public InputResponseDto handleInput(@DestinationVariable String sessionId, @DestinationVariable String userId, @Payload InputRequestDto inputRequestDto) {
-        // TODO: Implement the logic to handle the input request
-        return new InputResponseDto(true, 0);
+    public void handleInput(@DestinationVariable String sessionId, @DestinationVariable String userId, @Payload InputRequestDto inputRequestDto) {
+        InputResponseDto responseDto = sessionManager.getSessionObject(sessionId).getGameLoop().handleInput(userId, inputRequestDto);
+        template.convertAndSend(String.format("/game/%s/frameInfos/%s", sessionId, userId), responseDto);
     }
 }
