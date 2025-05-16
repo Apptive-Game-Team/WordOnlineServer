98eba03a7347448ed954a59970ebd3619c966961
Monolong <83206119+Monolong@users.noreply.github.com>
2025-05-16 15:04:01 +0900

    Update Raycast.java
    - 인터페이스로 변경

--- src/main/java/com/wordonline/server/game/util/Raycast.java
+++ src/main/java/com/wordonline/server/game/util/Raycast.java
@@ -2,12 +2,7 @@ package com.wordonline.server.game.util;
 
 import com.wordonline.server.game.domain.object.GameObject;
 import java.util.List;
-import java.util.ArrayList;
 
-
-public class Raycast {
-    public List<GameObject> circle(GameObject object, float distance)
-    {
-        return new ArrayList<GameObject>();
-    };
+public interface Raycast {
+    public List<GameObject> circle(GameObject object, float distance);
 }
