5c598d148d947c07ec32d5b247e00bf6b317a030
Yunseong <dev.yunseong@gmail.com>
2025-05-17 23:48:13 +0900

    Delete Raycast.java

--- src/main/java/com/wordonline/server/game/util/Raycast.java
+++
@@ -1,8 +0,0 @@
-package com.wordonline.server.game.util;
-
-import com.wordonline.server.game.domain.object.GameObject;
-import java.util.List;
-
-public interface Raycast {
-    public List<GameObject> circle(GameObject object, float distance);
-}
