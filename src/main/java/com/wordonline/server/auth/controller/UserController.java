8a51c2c6bd01180c18615df8620f2e3e39ee74e2
Yunseong <dev.yunseong@gmail.com>
2025-07-02 16:10:01 +0900

    refactor UserController - url 수정

--- src/main/java/com/wordonline/server/auth/controller/UserController.java
+++ src/main/java/com/wordonline/server/auth/controller/UserController.java
@@ -2,20 +2,21 @@ package com.wordonline.server.auth.controller;
 
 import com.wordonline.server.auth.domain.KakaoUser;
 import com.wordonline.server.auth.domain.PrincipalDetails;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("api/users/")
+@RequestMapping("/api/users")
 @RestController
 public class UserController {
-
-    @GetMapping("mine")
+    @GetMapping("/mine")
     public ResponseEntity<KakaoUser> getUser(@AuthenticationPrincipal PrincipalDetails principalDetails) {
-
+        if (principalDetails == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         return ResponseEntity.ok(principalDetails.user);
     }
-
 }
