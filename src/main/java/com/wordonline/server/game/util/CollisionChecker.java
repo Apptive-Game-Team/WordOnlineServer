600588ab7a29de7bd37720978574c575a58f4369
Monolong <tjdvlf0201@gmail.com>
2025-07-03 20:59:00 +0900

    Update CollisionChecker.java
    - 이제 충돌처리는 아군의 오브젝트끼리는 충돌이 되지 않습니다.

--- src/main/java/com/wordonline/server/game/util/CollisionChecker.java
+++ src/main/java/com/wordonline/server/game/util/CollisionChecker.java
@@ -2,10 +2,11 @@ package com.wordonline.server.game.util;
 
 import com.wordonline.server.game.domain.object.GameObject;
 import com.wordonline.server.game.domain.object.component.Collidable;
+import com.wordonline.server.game.dto.Master;
 
 public class CollisionChecker{
     public boolean isColliding(GameObject obj1, GameObject obj2) {
         double dist = obj1.getPosition().distance(obj2.getPosition());
-        return dist <= (obj1.getRadius() + obj2.getRadius());
+        return ((obj1.getMaster()!=obj2.getMaster()) || (obj1.getMaster() == Master.None && obj2.getMaster() == Master.None)) && dist <= (obj1.getRadius() + obj2.getRadius());
     }
 }
