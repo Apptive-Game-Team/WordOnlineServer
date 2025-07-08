ecc7154fe075858eea8178e35cf996d626278a45
Yunseong <dev.yunseong@gmail.com>
2025-07-08 19:06:59 +0900

    refactor: simplify collision detection logic and clean up unused imports

--- src/main/java/com/wordonline/server/game/domain/object/component/Collidable.java
+++ src/main/java/com/wordonline/server/game/domain/object/component/Collidable.java
@@ -3,5 +3,5 @@ package com.wordonline.server.game.domain.object.component;
 import com.wordonline.server.game.domain.object.GameObject;
 
 public interface Collidable {
-    public void onCollision(GameObject otherObject);
+    void onCollision(GameObject otherObject);
 }
