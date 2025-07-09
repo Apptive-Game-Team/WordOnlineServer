a344713ed831e5681c6390774f077b208d8d2cf2
Yunseong <dev.yunseong@gmail.com>
2025-07-09 20:06:26 +0900

    feature test 용 mock login 구현

--- src/main/java/com/wordonline/server/auth/controller/UserController.java
+++ src/main/java/com/wordonline/server/auth/controller/UserController.java
@@ -2,34 +2,66 @@ package com.wordonline.server.auth.controller;
 
 import com.wordonline.server.auth.domain.KakaoUser;
 import com.wordonline.server.auth.domain.PrincipalDetails;
+import com.wordonline.server.auth.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.naming.AuthenticationException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RequestMapping("/api/users")
 @RestController
+@RequiredArgsConstructor
 public class UserController {
 
-    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+    private final UserService userService;
+
+    private static AtomicInteger atomicInteger = new AtomicInteger(123);
 
     @GetMapping("/mine")
-    public ResponseEntity<KakaoUser> getUser(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+    public ResponseEntity<KakaoUser> getUser(@AuthenticationPrincipal PrincipalDetails principalDetails, HttpServletRequest request) throws AuthenticationException {
         if (principalDetails == null) {
 
-            // TODO - remove mock data
-            return ResponseEntity.ok(new KakaoUser(
+            KakaoUser kakaoUser = new KakaoUser(
                     atomicInteger.getAndIncrement(),
                     "mock email",
                     "demo-user",
                     "mock user"
-            ));
+            );
+
+            userService.loginOrRegisterUser(kakaoUser);
+
+            PrincipalDetails principal = new PrincipalDetails(kakaoUser);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    principal,
+                    null,
+                    principal.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            HttpSession session = request.getSession(true);
+            session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+
+
+            // TODO - remove mock data
+            return ResponseEntity.status(HttpStatus.CREATED).body(kakaoUser);
 
 //             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
+
+
         return ResponseEntity.ok(principalDetails.user);
     }
 }
