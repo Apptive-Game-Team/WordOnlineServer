7cee8de6ceecbac76dce1e20893226edba420dd5
Yunseong <dev.yunseong@gmail.com>
2025-07-10 11:55:09 +0900

    feature deck 정보를 받아오도록 구현

--- src/main/java/com/wordonline/server/game/domain/GameSessionData.java
+++ src/main/java/com/wordonline/server/game/domain/GameSessionData.java
@@ -4,19 +4,21 @@ import com.wordonline.server.game.domain.object.GameObject;
 import com.wordonline.server.game.dto.Master;
 import com.wordonline.server.game.service.CardDeck;
 import com.wordonline.server.game.service.ManaCharger;
+import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 // this class is used to store the game session data
+@RequiredArgsConstructor
 public class GameSessionData {
     public final PlayerData leftPlayerData = new PlayerData();
     public final PlayerData rightPlayerData = new PlayerData();
     public final List<GameObject> gameObjects = new ArrayList<>();
     public final List<GameObject> gameObjectsToAdd = new ArrayList<>();
 
-    public final CardDeck leftCardDeck = new CardDeck();
-    public final CardDeck rightCardDeck = new CardDeck();
+    public final CardDeck leftCardDeck;
+    public final CardDeck rightCardDeck;
     public final ManaCharger manaCharger = new ManaCharger();
 
     public PlayerData getPlayerData(Master master){
--- src/main/java/com/wordonline/server/game/service/CardDeck.java
+++ src/main/java/com/wordonline/server/game/service/CardDeck.java
@@ -3,26 +3,24 @@ package com.wordonline.server.game.service;
 import com.wordonline.server.game.domain.magic.CardType;
 import com.wordonline.server.game.domain.PlayerData;
 import com.wordonline.server.game.dto.CardInfoDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 // CardDeck class to manage the deck of cards
+@Slf4j
+@RequiredArgsConstructor
 public class CardDeck {
-    Random rand = new Random();
 
-    // Dummy cards for testing
-    // TODO: Replace with actual card data
-    List<CardType> cards = new ArrayList<>(List.of( CardType.Shoot, CardType.Summon,
-            CardType.Shoot, CardType.Explode, CardType.Spawn, CardType.Rock, CardType.Leaf,
-    CardType.Water, CardType.Lightning, CardType.Fire));
+    private final Random rand = new Random();
+    public final List<CardType> cards;
 
     // random pick card and update to Player Data, Frame Info Dto
     public void drawCard(PlayerData player, CardInfoDto cardInfoDto) {
         if (cards.isEmpty() || player.cards.size() >= PlayerData.MAX_CARD_NUM)
             return;
-
         int cardIndex = rand.nextInt(cards.size());
         CardType card = cards.get(cardIndex);
         if (player.addCard(card)){ // Added
