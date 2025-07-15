312f49fb9d4d2e3640dc4158de9fdfd70821de1a
Yunseong <dev.yunseong@gmail.com>
2025-07-15 23:17:40 +0900

    feature MmrService 구현 - GameLoop에 주입

--- src/main/java/com/wordonline/server/game/component/SessionManager.java
+++ src/main/java/com/wordonline/server/game/component/SessionManager.java
@@ -2,17 +2,22 @@ package com.wordonline.server.game.component;
 
 import com.wordonline.server.game.domain.SessionObject;
 import com.wordonline.server.game.service.GameLoop;
+import com.wordonline.server.game.service.MmrService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 // this class is used to manage the sessions
 @Service
+@RequiredArgsConstructor
 public class SessionManager {
+
     private static Map<String, SessionObject> sessions = new java.util.concurrent.ConcurrentHashMap<>();
+    private final MmrService mmrService;
 
     public void createSession(SessionObject sessionObject) {
-        GameLoop gameLoop = new GameLoop(sessionObject);
+        GameLoop gameLoop = new GameLoop(sessionObject, mmrService);
         sessionObject.setGameLoop(gameLoop);
         Thread thread = new Thread(gameLoop);
         thread.start();
