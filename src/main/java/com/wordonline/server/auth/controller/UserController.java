89942418ee584579c1e4f465e24259f1253e14d3
Yunseong <dev.yunseong@gmail.com>
2025-07-16 12:29:38 +0900

    feature validation 추가

--- src/main/java/com/wordonline/server/auth/controller/UserController.java
+++ src/main/java/com/wordonline/server/auth/controller/UserController.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.naming.AuthenticationException;
@@ -28,21 +29,21 @@ public class UserController {
 
     @PostMapping("")
     public ResponseEntity<AuthResponseDto> registerAndLogin(
-            @RequestBody UserRegisterRequestDto userRegisterRequestDto
+            @Validated @RequestBody UserRegisterRequestDto userRegisterRequestDto
             ) throws AuthenticationException {
         return ResponseEntity.ok(authService.registerAndLogin(userRegisterRequestDto));
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponseDto> login(
-            @RequestBody UserLoginRequestDto userLoginRequestDto
+            @Validated @RequestBody UserLoginRequestDto userLoginRequestDto
     ) throws AuthenticationException {
         return ResponseEntity.ok(authService.login(userLoginRequestDto));
     }
 
 
     @GetMapping("/mine")
-    public ResponseEntity<UserResponseDto> getUser(@AuthenticationPrincipal PrincipalDetails principalDetails, HttpServletRequest request) throws AuthenticationException {
+    public ResponseEntity<UserResponseDto> getUser(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         if (principalDetails == null) {
              return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
