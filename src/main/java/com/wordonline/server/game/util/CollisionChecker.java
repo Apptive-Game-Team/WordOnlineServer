631054f2b6fa0dfa9a7831322c93e559108f45e4
Yunseong <dev.yunseong@gmail.com>
2025-07-17 14:13:51 +0900

    feature CollisionChecker - static 하게 수정, 로직 변경

--- src/main/java/com/wordonline/server/game/util/CollisionChecker.java
+++ src/main/java/com/wordonline/server/game/util/CollisionChecker.java
@@ -4,12 +4,14 @@ import com.wordonline.server.game.domain.object.GameObject;
 import com.wordonline.server.game.dto.Master;
 
 public class CollisionChecker{
-    public boolean isColliding(GameObject obj1, GameObject obj2) {
+    public static boolean isColliding(GameObject obj1, GameObject obj2) {
         if (obj1.getMaster() == obj2.getMaster() &&
                 obj1.getMaster() != Master.None) {
             return false;
         }
-        double dist = obj1.getPosition().distance(obj2.getPosition());
-        return  dist <= (obj1.getRadius() + obj2.getRadius());
+
+        return obj1.getColliders().stream()
+            .anyMatch(a -> obj2.getColliders().stream()
+                    .anyMatch(a::isCollidingWish));
     }
 }
