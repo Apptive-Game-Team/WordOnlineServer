dcd5b5c1c7221613fcd2903c6fb1c84b85310dac
Yunseong <dev.yunseong@gmail.com>
2025-07-17 14:14:43 +0900

    feature: integrate colliders into game objects and update prefab types

--- src/main/java/com/wordonline/server/game/domain/object/PrefabType.java
+++ src/main/java/com/wordonline/server/game/domain/object/PrefabType.java
@@ -13,30 +13,34 @@ import com.wordonline.server.game.domain.object.component.magic.Explode;
 import com.wordonline.server.game.domain.object.component.magic.Shot;
 import com.wordonline.server.game.domain.object.component.magic.Spawner;
 import com.wordonline.server.game.domain.object.component.mob.statemachine.slime.Slime;
+import com.wordonline.server.game.domain.object.component.physic.CircleCollider;
+import com.wordonline.server.game.domain.object.component.physic.RigidBody;
 import com.wordonline.server.game.dto.Effect;
 
 public enum PrefabType {
     // fire
     FireShot((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getComponents().add(new RigidBody(gameObject, 1));
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.FIRE);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Burn));
         gameObject.getComponents().add(new Shot(gameObject, 10));
     })),
     FireExplode((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.FIRE);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Burn));
         gameObject.getComponents().add(new Explode(gameObject, 8));
     })),
     FireField((gameObject) -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.FIRE);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Burn));
         gameObject.getComponents().add(new TimedSelfDestroyer(gameObject, 5f));
     }),
     FireSlime((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getComponents().add(new RigidBody(gameObject, 1));
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.FIRE);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Burn));
         gameObject.getComponents().add(new Slime(gameObject, 8, 0.8f, 3));
@@ -44,32 +48,33 @@ public enum PrefabType {
         gameObject.getComponents().add(new FireEffectReceiver(gameObject));
     })),
     FireSummon((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.FIRE);
         gameObject.getComponents().add(new Spawner(gameObject, 5, PrefabType.FireSlime));
     })),
 
     // water
     WaterShot((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.WATER);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Wet));
         gameObject.getComponents().add(new Shot(gameObject, 10));
     })),
     WaterExplode((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.WATER);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Wet));
         gameObject.getComponents().add(new Explode(gameObject, 8));
     })),
     WaterField((gameObject) -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.WATER);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Wet));
         gameObject.getComponents().add(new TimedSelfDestroyer(gameObject, 5f));
     }),
     WaterSlime((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getComponents().add(new RigidBody(gameObject, 1));
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Wet));
         gameObject.getComponents().add(new Slime(gameObject, 8, 0.8f, 3));
         gameObject.getComponents().add(new PathSpawner(gameObject, PrefabType.WaterField, 1f));
@@ -77,82 +82,85 @@ public enum PrefabType {
         gameObject.getComponents().add(new CommonEffectReceiver(gameObject));
     })),
     WaterSummon((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.WATER);
         gameObject.getComponents().add(new Spawner(gameObject, 5, PrefabType.WaterSlime));
     })),
 
     // rock
     RockShot((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.ROCK);
         gameObject.getComponents().add(new Shot(gameObject, 10));
     })),
     RockExplode((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.ROCK);
         gameObject.getComponents().add(new Explode(gameObject, 8));
     })),
     RockSlime((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getComponents().add(new RigidBody(gameObject, 1));
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.getComponents().add(new Slime(gameObject, 8, 0.8f, 3));
         gameObject.setElement(ElementType.ROCK);
         gameObject.getComponents().add(new CommonEffectReceiver(gameObject));
     })),
     RockSummon((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.ROCK);
         gameObject.getComponents().add(new Spawner(gameObject, 5, PrefabType.RockSlime));
     })),
 
     // electric
     ElectricShot((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.LIGHTING);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Shock));
         gameObject.getComponents().add(new Shot(gameObject, 10));
     })),
     ElectricExplode((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.LIGHTING);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Shock));
         gameObject.getComponents().add(new Explode(gameObject, 8));
     })),
     ElectricSlime((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getComponents().add(new RigidBody(gameObject, 1));
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Shock));
         gameObject.getComponents().add(new Slime(gameObject, 8, 0.8f, 3));
         gameObject.setElement(ElementType.LIGHTING);
         gameObject.getComponents().add(new CommonEffectReceiver(gameObject));
     })),
     ElectricSummon((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.LIGHTING);
         gameObject.getComponents().add(new Spawner(gameObject, 5, ElectricSlime));
     })),
 
     // leaf
     LeafShot((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.LEAF);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Snared));
         gameObject.getComponents().add(new Shot(gameObject, 10));
     })),
     LeafExplode((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.LEAF);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Snared));
         gameObject.getComponents().add(new Explode(gameObject, 8));
     })),
     LeafField((gameObject) -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.LEAF);
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Snared));
         gameObject.getComponents().add(new LeafFieldEffectReceiver(gameObject));
         gameObject.getComponents().add(new TimedSelfDestroyer(gameObject, 5f));
     }),
     LeafSlime((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getComponents().add(new RigidBody(gameObject, 1));
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.getComponents().add(new EffectProvider(gameObject, Effect.Snared));
         gameObject.getComponents().add(new Slime(gameObject, 8, 0.8f, 3));
         gameObject.getComponents().add(new PathSpawner(gameObject, PrefabType.LeafField, 1f));
@@ -160,7 +168,7 @@ public enum PrefabType {
         gameObject.getComponents().add(new CommonEffectReceiver(gameObject));
     })),
     LeafSummon((gameObject -> {
-        gameObject.setRadius(0.5f);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 0.5f));
         gameObject.setElement(ElementType.LEAF);
         gameObject.getComponents().add(new Spawner(gameObject, 5, PrefabType.LeafSlime));
     })),
@@ -171,7 +179,7 @@ public enum PrefabType {
     }),
 
     Player((gameObject)-> {
-        gameObject.setRadius(1);
+        gameObject.getColliders().add(new CircleCollider(gameObject, 1));
         gameObject.setElement(ElementType.NONE);
         gameObject.getComponents().add(new PlayerHealthComponent(gameObject));
         gameObject.getComponents().add(new CommonEffectReceiver(gameObject));
