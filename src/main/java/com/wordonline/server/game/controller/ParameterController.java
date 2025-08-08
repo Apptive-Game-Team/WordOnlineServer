22db47dede68729024fe127130f9800fb68d1224
Yunseong <dev.yunseong@gmail.com>
2025-08-08 19:29:05 +0900

    Change invalidate cache endpoint to POST method in ParameterController

--- src/main/java/com/wordonline/server/game/controller/ParameterController.java
+++ src/main/java/com/wordonline/server/game/controller/ParameterController.java
@@ -2,9 +2,7 @@ package com.wordonline.server.game.controller;
 
 import com.wordonline.server.game.service.ParameterService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -13,7 +11,7 @@ public class ParameterController {
 
     private final ParameterService parameterService;
 
-    @GetMapping("/invalidate")
+    @PostMapping("/invalidate")
     public void invalidateParameterCache() {
         parameterService.invalidateCache();
     }
