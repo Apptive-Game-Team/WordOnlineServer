c8520c1dc72e0dc3799cc26316f9847a37c9c597
Yunseong <dev.yunseong@gmail.com>
2025-08-13 13:46:32 +0900

    Enhance application health logging by adding CPU and memory usage metrics in ApplicationLogger

--- src/main/java/com/wordonline/server/config/ApplicationLogger.java
+++ src/main/java/com/wordonline/server/config/ApplicationLogger.java
@@ -1,5 +1,6 @@
 package com.wordonline.server.config;
 
+import com.sun.management.OperatingSystemMXBean;
 import com.wordonline.server.game.component.SessionManager;
 import com.wordonline.server.matching.component.MatchingManager;
 import lombok.RequiredArgsConstructor;
@@ -7,17 +8,40 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.lang.management.ManagementFactory;
+
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ApplicationLogger {
-    private final long LOGGING_INTERVAL = 1000 * 60 * 30;// ms // 30 min
+    private final long LOGGING_INTERVAL = 1000 * 60 * 10; // 10 min [ms]
 
     private final MatchingManager matchingManager;
     private final SessionManager sessionManager;
+    private final OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
 
     @Scheduled(fixedRate = LOGGING_INTERVAL)
     public void logApplicationHealth() {
-        log.info("[Health Check] {} | Sessions {}", matchingManager.getHealthLog(), sessionManager.getHealthLog());
+
+        log.info("[Health Check] {} | {} | {} | {}",
+                getCpuLog(), getMemoryLog(),
+                matchingManager.getHealthLog(),
+                sessionManager.getHealthLog());
+    }
+
+    private String getCpuLog() {
+        double cpuLoad = operatingSystemMXBean.getCpuLoad() * 100;
+        return String.format("CPU Usage: %.2f", cpuLoad);
+    }
+
+    private String getMemoryLog() {
+        long totalPhysicalMemory = operatingSystemMXBean.getTotalMemorySize();
+        long freePhysicalMemory = operatingSystemMXBean.getFreeMemorySize();
+        long usedPhysicalMemory = totalPhysicalMemory - freePhysicalMemory;
+
+        return String.format("Used Memory: %.2f GB / %.2f GB",
+                usedPhysicalMemory / 1024.0 / 1024.0 / 1024.0,
+                totalPhysicalMemory / 1024.0 / 1024.0 / 1024.0);
     }
 }
