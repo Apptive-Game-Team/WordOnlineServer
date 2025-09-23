edb37338b71364880f981a48b938bfae24978331
Monolong <83206119+Monolong@users.noreply.github.com>
2025-09-24 02:01:42 +0900

    Refactor(ZPhysics) : Refactor ZPhysics method
    - 리지드바디에서 Z축 이동 메소드들을 빼서 ZPhysics 클래스에 책임을 부여했습니다.

---
+++ src/main/java/com/wordonline/server/game/domain/object/component/physic/ZPhysics.java
@@ -0,0 +1,66 @@
+package com.wordonline.server.game.domain.object.component.physic;
+
+import com.wordonline.server.game.config.GameConfig;
+import com.wordonline.server.game.domain.AttackInfo;
+import com.wordonline.server.game.domain.magic.ElementType;
+import com.wordonline.server.game.domain.object.GameObject;
+import com.wordonline.server.game.domain.object.Vector3;
+import com.wordonline.server.game.domain.object.component.Component;
+import com.wordonline.server.game.domain.object.component.mob.Mob;
+import lombok.Setter;
+
+import java.util.function.Supplier;
+
+public class ZPhysics extends Component {
+
+    private float zVelocity;
+    @Setter
+    private float groundZ = 0f;
+    @Setter
+    private float gravity = GameConfig.GRAVITY_ACCEL;
+
+
+    @Setter
+    private float fallThreshold = GameConfig.FALL_THRESHOLD;
+    @Setter
+    private float fallDamageVelocityUnit = GameConfig.FALL_THRESHOLD_VELOCITY;
+
+    public ZPhysics(GameObject go) {
+        super(go);
+    }
+
+    public void addImpulseZ(float force) {
+        zVelocity += force;
+    }
+
+    public void applyZForce() {
+        final float dt = gameObject.getGameLoop().deltaTime;
+
+        zVelocity -= gravity * dt;
+
+        Vector3 p = gameObject.getPosition();
+        float z = p.getZ() + zVelocity * dt;
+
+        if (z < groundZ) {
+            z = groundZ;
+            if (zVelocity < 0f && Math.abs(p.getZ() - groundZ) > fallThreshold) {
+                int fallDamage = (int) Math.floor(Math.abs(zVelocity) / fallDamageVelocityUnit);
+                Mob mob = gameObject.getComponent(Mob.class);
+                if(mob != null){
+                    mob.applyDamage(new AttackInfo(fallDamage, ElementType.NONE));
+                }
+            }
+            zVelocity = 0f;
+        }
+        gameObject.setPosition(new Vector3(p.getX(), p.getY(), z));
+    }
+
+    @Override
+    public void start() {}
+
+    @Override
+    public void update() {}
+
+    @Override
+    public void onDestroy() {}
+}
