a7204827c4ac28f0c2cb55a71564a4af900e4eb5
Monolong <tjdvlf0201@gmail.com>
2025-09-24 16:12:52 +0900

    feature(lockHover) : Add Lock Hovering
    - 공중 유닛 호버 기능을 잠그는 메소드를 추가했습니다.

--- src/main/java/com/wordonline/server/game/domain/object/component/effect/ShockStatusEffect.java
+++ src/main/java/com/wordonline/server/game/domain/object/component/effect/ShockStatusEffect.java
@@ -4,10 +4,11 @@ import com.wordonline.server.game.domain.magic.ElementType;
 import com.wordonline.server.game.domain.object.GameObject;
 import com.wordonline.server.game.domain.object.component.mob.Mob;
 import com.wordonline.server.game.domain.object.component.physic.RigidBody;
+import com.wordonline.server.game.domain.object.component.physic.ZPhysics;
 import com.wordonline.server.game.dto.Effect;
 
 public class ShockStatusEffect extends BaseStatusEffect {
-    private static final float STUN_DURATION = 0.5f;
+    private static final float STUN_DURATION = 1f;
 
     public ShockStatusEffect(GameObject owner) {
         super(owner, STUN_DURATION);
@@ -17,8 +18,12 @@ public class ShockStatusEffect extends BaseStatusEffect {
     @Override
     public void start() {
         Mob mob = gameObject.getComponent(Mob.class);
+        ZPhysics zp = gameObject.getComponent(ZPhysics.class);
         if (mob != null) {
             mob.getSpeed().setModifierPercent(-1f);
+        }
+        if (zp != null) {
+            zp.LockHover(this);
         }
     }
 
@@ -33,6 +38,10 @@ public class ShockStatusEffect extends BaseStatusEffect {
         Mob mob = gameObject.getComponent(Mob.class);
         if (mob != null) {
             mob.getSpeed().setModifierPercent(0f);
+        }
+        ZPhysics zp = gameObject.getComponent(ZPhysics.class);
+        if (zp != null) {
+            zp.UnlockHover(this);
         }
         super.expire();
     }
