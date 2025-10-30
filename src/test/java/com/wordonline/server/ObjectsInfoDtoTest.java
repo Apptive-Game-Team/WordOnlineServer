6bb81d5492f51b8c0f74422c245d112b7a4e7785
Yunseong <dev.yunseong@gmail.com>
2025-10-31 01:33:50 +0900

    test: rename test method to reflect projectile DTO functionality

--- src/test/java/com/wordonline/server/ObjectsInfoDtoTest.java
+++ src/test/java/com/wordonline/server/ObjectsInfoDtoTest.java
@@ -1,7 +1,6 @@
 package com.wordonline.server;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
