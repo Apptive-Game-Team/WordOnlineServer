76851eb531d6ef9e7a46eebc295f624d3078a7e3
Yunseong <dev.yunseong@gmail.com>
2025-11-11 17:16:13 +0900

    fix(CardDeck): refactor card management to use ConcurrentLinkedDeque and simplify card drawing logic

--- src/main/java/com/wordonline/server/game/service/CardDeck.java
+++ src/main/java/com/wordonline/server/game/service/CardDeck.java
@@ -3,23 +3,29 @@ package com.wordonline.server.game.service;
 import com.wordonline.server.game.domain.magic.CardType;
 import com.wordonline.server.game.domain.PlayerData;
 import com.wordonline.server.game.dto.CardInfoDto;
-import com.wordonline.server.game.dto.frame.FrameInfoDto;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 // CardDeck class to manage the deck of cards
 @Slf4j
-@RequiredArgsConstructor
 public class CardDeck {
 
     private static final float CARD_DRAW_INTERVAL = 1;
+    private final Queue<CardType> cards;
 
-    private final Random rand = new Random();
-    public final List<CardType> cards;
+    public CardDeck(List<CardType> cards) {
+        Collections.shuffle(cards);
+        this.cards = new ConcurrentLinkedDeque<>(cards);
+    }
+
+    public void returnCards(List<CardType> cards) {
+        this.cards.addAll(cards);
+    }
 
     // random pick card and update to Player Data, Frame Info Dto
     public void drawCard(PlayerData player, CardInfoDto cardInfoDto, int frameNum) {
@@ -27,11 +33,11 @@ public class CardDeck {
             return;
         if (cards.isEmpty() || player.cards.size() >= PlayerData.MAX_CARD_NUM)
             return;
-        int cardIndex = rand.nextInt(cards.size());
-        CardType card = cards.get(cardIndex);
+
+        CardType card = cards.remove();
+
         if (player.addCard(card)){ // Added
             cardInfoDto.addCard(card);
-            cards.remove(cardIndex);
         }
     }
 }
