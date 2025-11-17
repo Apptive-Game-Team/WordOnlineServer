145d2e2b8ff37627576201bf2b5385734aee3c67
Yunseong <dev.yunseong@gmail.com>
2025-11-17 18:17:10 +0900

    refactor change import and package

--- src/main/java/com/wordonline/server/game/domain/object/component/mob/PlayerHealthComponent.java
+++ src/main/java/com/wordonline/server/game/domain/object/component/mob/PlayerHealthComponent.java
@@ -1,9 +1,8 @@
-package com.wordonline.server.game.domain.object.component;
+package com.wordonline.server.game.domain.object.component.mob;
 
 import com.wordonline.server.game.domain.AttackInfo;
 import com.wordonline.server.game.domain.PlayerData;
 import com.wordonline.server.game.domain.object.GameObject;
-import com.wordonline.server.game.domain.object.component.mob.Mob;
 import com.wordonline.server.game.dto.Master;
 
 public class PlayerHealthComponent extends Mob {
--- src/main/java/com/wordonline/server/game/domain/object/prefab/implement/misc/ManaWellPrefabInitializer.java
+++ src/main/java/com/wordonline/server/game/domain/object/prefab/implement/misc/ManaWellPrefabInitializer.java
@@ -4,7 +4,7 @@ import com.wordonline.server.game.domain.AttackInfo;
 import com.wordonline.server.game.domain.Parameters;
 import com.wordonline.server.game.domain.magic.ElementType;
 import com.wordonline.server.game.domain.object.GameObject;
-import com.wordonline.server.game.domain.object.component.SelfAttacker;
+import com.wordonline.server.game.domain.object.component.mob.component.SelfAttacker;
 import com.wordonline.server.game.domain.object.component.effect.receiver.CommonEffectReceiver;
 import com.wordonline.server.game.domain.object.component.mob.ManaWellMob;
 import com.wordonline.server.game.domain.object.component.physic.CircleCollider;
