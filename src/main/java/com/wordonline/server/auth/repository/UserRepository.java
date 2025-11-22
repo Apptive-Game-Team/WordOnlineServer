e18242aece538a1276635ec3ea23dd923d2c0818
Yunseong <dev.yunseong@gmail.com>
2025-11-22 21:34:42 +0900

    refactor(UserRepository): remove unused methods and simplify user status update logic

--- src/main/java/com/wordonline/server/auth/repository/UserRepository.java
+++ src/main/java/com/wordonline/server/auth/repository/UserRepository.java
@@ -1,50 +1,25 @@
 package com.wordonline.server.auth.repository;
 
-import com.wordonline.server.auth.domain.User;
 import com.wordonline.server.auth.domain.UserStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.simple.JdbcClient;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
 public class UserRepository {
 
-    private static final String DELETE_USER_BY_ID = """
-            DELETE FROM users
-            WHERE id = :id;
-            """;
-    private static final String GET_USER_BY_ID = """
-            SELECT id, status, selected_deck_id
-            FROM users
-            WHERE id = :id;
-            """;
     private static final String GET_SELECTED_DECK_ID_BY_ID = """
             SELECT selected_deck_id
             FROM users
             WHERE id = :userId;
             """;
-    private static final String SAVE_USER = """
-            INSERT INTO users(id)
-            VALUES
-            (:id)
-            RETURNING id;
-            """;
 
     private static final String UPDATE_STATUS = """
         UPDATE users
-           SET status = CAST(:status AS user_status)
-         WHERE id = :id;
-        """;
-
-    private static final String GET_STATUS = """
-        SELECT status
-          FROM users
+           SET status = :status
          WHERE id = :id;
         """;
 
@@ -61,12 +36,6 @@ public class UserRepository {
             """;
 
     private final JdbcClient jdbcClient;
-
-    public boolean deleteById(long userId) {
-        return jdbcClient.sql(DELETE_USER_BY_ID)
-                .param("id", userId)
-                .update() >= 1;
-    }
 
     public Optional<Long> getSelectedDeckId(long userId) {
         return jdbcClient.sql(GET_SELECTED_DECK_ID_BY_ID)
@@ -89,41 +58,10 @@ public class UserRepository {
                 .update();
     }
 
-    public Optional<User> findUserById(long id) {
-        return jdbcClient.sql(GET_USER_BY_ID)
-                .param("id", id)
-                .query((rs, num) ->
-                        new User(
-                                rs.getLong("id"),
-                                UserStatus.valueOf(rs.getString("status")),
-                                rs.getLong("selected_deck_id")
-                        ))
-                .optional();
-    }
-
-    public Long saveUser(long memberId) {
-        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
-
-        jdbcClient.sql(SAVE_USER)
-                .param("id", memberId)
-                .update(keyHolder);
-
-        return keyHolder.getKey().longValue();
-    }
-
     public void updateStatus(long userId, UserStatus status) {
         jdbcClient.sql(UPDATE_STATUS)
                 .param("id", userId)
                 .param("status", status.name())
                 .update();
-    }
-
-    public UserStatus getStatus(long userId) {
-        return jdbcClient.sql(GET_STATUS)
-                .param("id", userId)
-                .query((rs, rowNum) ->
-                        UserStatus.valueOf(rs.getString("status"))
-                )
-                .single();
     }
 }
