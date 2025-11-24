606c51280b7045b5bade496a11bca2cb56c136f8
Yunseong <dev.yunseong@gmail.com>
2025-11-24 10:42:57 +0900

    refactor(Physics): add default method for overlapSphereAll using GameObject position

--- src/main/java/com/wordonline/server/game/util/Physics.java
+++ src/main/java/com/wordonline/server/game/util/Physics.java
@@ -2,10 +2,16 @@ package com.wordonline.server.game.util;
 
 import com.wordonline.server.game.domain.object.GameObject;
 import com.wordonline.server.game.domain.object.Vector2;
+import com.wordonline.server.game.domain.object.Vector3;
 
 import java.util.List;
 
 public interface Physics {
-    List<GameObject> overlapSphereAll(GameObject object, float radius);
+
+    default List<GameObject> overlapSphereAll(GameObject object, float radius) {
+        return overlapSphereAll(object.getPosition(), radius);
+    }
+
+    List<GameObject> overlapSphereAll(Vector3 position, float radius);
     GameObject raycast(GameObject object, Vector2 direction, float distance);
 }
