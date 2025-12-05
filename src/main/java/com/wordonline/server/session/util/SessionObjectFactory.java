f6624cebb7ed3c197df44ebdf703a7136452ceef
Yunseong <dev.yunseong@gmail.com>
2025-12-05 17:05:36 +0900

    fix(BotAction, SessionObjectFactory): update user ID handling and log bot deck selection

--- src/main/java/com/wordonline/server/session/util/SessionObjectFactory.java
+++ src/main/java/com/wordonline/server/session/util/SessionObjectFactory.java
@@ -1,15 +1,20 @@
 package com.wordonline.server.session.util;
 
+import java.util.List;
+
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 import com.wordonline.server.deck.service.DeckService;
 import com.wordonline.server.game.domain.SessionObject;
 import com.wordonline.server.game.domain.SessionType;
+import com.wordonline.server.game.domain.magic.CardType;
 import com.wordonline.server.session.dto.SessionDto;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SessionObjectFactory {
@@ -39,13 +44,15 @@ public class SessionObjectFactory {
         long leftUserId = sessionDto.uid1();
         long rightUserId = sessionDto.uid2();
 
+        List<CardType> botDeck = deckService.getSelectedCards(rightUserId);
+        log.trace("[Bot Deck] {}", botDeck);
         return new SessionObject(
                 sessionDto.sessionId(),
                 leftUserId,
                 rightUserId,
                 simpMessagingTemplate,
                 deckService.getSelectedCards(leftUserId),
-                deckService.getSelectedCards(leftUserId),
+                botDeck,
                 SessionType.Practice
         );
     }
