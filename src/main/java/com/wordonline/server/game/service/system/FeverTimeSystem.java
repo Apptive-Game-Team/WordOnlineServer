76104aa59206651ee8e5d9547f7c6619aeff1f28
Yunseong <dev.yunseong@gmail.com>
2026-01-09 13:19:51 +0900

    feat(FeverTimeSystem): add logging for fever time activation

--- src/main/java/com/wordonline/server/game/service/system/FeverTimeSystem.java
+++ src/main/java/com/wordonline/server/game/service/system/FeverTimeSystem.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 
 import com.wordonline.server.game.service.GameContext;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 @Scope("prototype")
 public class FeverTimeSystem implements GameSystem {
@@ -15,6 +18,7 @@ public class FeverTimeSystem implements GameSystem {
     public void update(GameContext gameContext) {
 
         if (!isFeverTime && gameContext.getGameTimer().isFeverTime()) {
+            log.info("[Log] startFeverTime");
             gameContext.getGameSessionData().leftCardDeck.fever();
             gameContext.getGameSessionData().rightCardDeck.fever();
             gameContext.getGameSessionData().leftPlayerData.manaCharger.updateManaCharge(1);
