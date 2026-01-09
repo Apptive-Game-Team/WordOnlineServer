c340ea7f8d197c48e4773dc59ee3eb9320128233
Yunseong <dev.yunseong@gmail.com>
2026-01-09 18:28:02 +0900

    fix(PlayerHealthComponent): rename onDamaged to applyDamage for clarity

--- src/main/java/com/wordonline/server/game/domain/object/component/mob/simple/PlayerHealthComponent.java
+++ src/main/java/com/wordonline/server/game/domain/object/component/mob/simple/PlayerHealthComponent.java
@@ -11,7 +11,7 @@ public class PlayerHealthComponent extends Mob {
     private PlayerData playerData;
 
     @Override
-    public void onDamaged(AttackInfo attackInfo) {
+    public void applyDamage(AttackInfo attackInfo) {
         super.onDamaged(attackInfo);
         playerData.hp = hp;
     }
