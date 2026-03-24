9f33c909127956a15d83c55be578cd7c01431749
Yunseong <dev.yunseong@gmail.com>
2026-03-24 16:15:30 +0900

    feat: implement magma and water explosion prefabs with enhanced damage handling

---
+++ src/main/java/com/wordonline/server/game/domain/object/component/magic/WaterExplode.java
@@ -0,0 +1,25 @@
+package com.wordonline.server.game.domain.object.component.magic;
+
+import com.wordonline.server.game.domain.object.GameObject;
+import com.wordonline.server.game.domain.object.component.physic.ZPhysics;
+
+public class WaterExplode extends Explode {
+
+    private final float zForce;
+
+    public WaterExplode(GameObject gameObject,
+            int damage,
+            float radius,
+            float zForce) {
+        super(gameObject, damage, radius);
+        this.zForce = zForce;
+    }
+
+    @Override
+    protected void handleGameObject(GameObject targetObject) {
+        super.handleGameObject(targetObject);
+
+        targetObject.getComponentOptional(ZPhysics.class)
+                .ifPresent(zPhysics -> zPhysics.addImpulseZ(zForce));
+    }
+}
