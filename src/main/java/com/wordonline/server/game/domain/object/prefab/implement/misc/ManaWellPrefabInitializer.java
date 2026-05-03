f2bfd55d9a5bbe99e8a265b72e05be11631b4925
Yunseong <dev.yunseong@gmail.com>
2026-05-03 16:28:25 +0900

    feat(prefab): replace SelfAttacker with TimedSelfDestroyer in multiple initializers

--- src/main/java/com/wordonline/server/game/domain/object/prefab/implement/misc/ManaWellPrefabInitializer.java
+++ src/main/java/com/wordonline/server/game/domain/object/prefab/implement/misc/ManaWellPrefabInitializer.java
@@ -1,10 +1,9 @@
 package com.wordonline.server.game.domain.object.prefab.implement.misc;
 
-import com.wordonline.server.game.domain.AttackInfo;
 import com.wordonline.server.game.domain.Parameters;
 import com.wordonline.server.game.domain.magic.ElementType;
 import com.wordonline.server.game.domain.object.GameObject;
-import com.wordonline.server.game.domain.object.component.mob.component.SelfAttacker;
+import com.wordonline.server.game.domain.object.component.TimedSelfDestroyer;
 import com.wordonline.server.game.domain.object.component.effect.receiver.CommonEffectReceiver;
 import com.wordonline.server.game.domain.object.component.mob.simple.ManaWellMob;
 import com.wordonline.server.game.domain.object.component.physic.CircleCollider;
@@ -30,11 +29,10 @@ public class ManaWellPrefabInitializer extends PrefabInitializer {
         gameObject.getComponents().add(new ManaWellMob(gameObject,
                 (int) parameters.getValue("mana_well", "hp")
         ));
-        gameObject.addComponent(new SelfAttacker(
+        gameObject.addComponent(new TimedSelfDestroyer(
                 gameObject,
-                new AttackInfo((int) parameters.getValue("mana_well", "damage"), ElementType.NONE),
-                (float) parameters.getValue("mana_well", "attack_interval")
-                ));
+                (int)(parameters.getValue("mana_well", "hp") * parameters.getValue("mana_well", "attack_interval") / parameters.getValue("mana_well", "damage"))
+        ));
         gameObject.setElement(EnumSet.of(ElementType.LIGHTNING, ElementType.NATURE));
         gameObject.getComponents().add(new CommonEffectReceiver(gameObject));
     }
