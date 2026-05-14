dbdeb705cb88d4671032da55a9934631c9e5af1d
Monolong <83206119+Monolong@users.noreply.github.com>
2026-05-14 14:54:28 +0900

    Feature/249 (#250) (#251)
    * feautre(newmagic)
    
    - 전기 폭발 추가
    - 칼바람 회오리 추가
    
    * Update razor_gale.sql
    
    - 범위 수정

---
+++ src/main/resources/sql/magic/lightning_explosion.sql
@@ -0,0 +1,32 @@
+WITH inserted_magic AS (
+    INSERT INTO magics(name)
+    SELECT 'lightning_explosion'
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM magics
+        WHERE name = 'lightning_explosion'
+    )
+    RETURNING id
+),
+target_magic AS (
+    SELECT id FROM inserted_magic
+    UNION ALL
+    SELECT id
+    FROM magics
+    WHERE name = 'lightning_explosion'
+),
+target_cards AS (
+    SELECT id
+    FROM cards
+    WHERE name IN ('Lightning', 'Explode')
+)
+INSERT INTO magic_cards(magic_id, card_id)
+SELECT tm.id, tc.id
+FROM target_magic tm
+CROSS JOIN target_cards tc
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM magic_cards mc
+    WHERE mc.magic_id = tm.id
+      AND mc.card_id = tc.id
+);
