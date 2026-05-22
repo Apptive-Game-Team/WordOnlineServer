66c1d601c786f3006d7f21b37949563db02cc38a
Yunseong Jeong <dev.yunseong@gmail.com>
2026-05-23 01:30:21 +0900

    fix: 타워/캐논 공격 판정 분리 및 지면 스폰 정리 (#258)
    * fix(tower): split tower and cannon attacks
    
    * chore(gitignore): ignore local handoffs and SQL scratch file

--- src/main/java/com/wordonline/server/game/domain/object/component/mob/detector/ClosestEnemyDetector.java
+++ src/main/java/com/wordonline/server/game/domain/object/component/mob/detector/ClosestEnemyDetector.java
@@ -3,15 +3,20 @@ package com.wordonline.server.game.domain.object.component.mob.detector;
 import com.wordonline.server.game.domain.object.GameObject;
 import com.wordonline.server.game.domain.object.component.Damageable;
 import com.wordonline.server.game.service.GameContext;
-import com.wordonline.server.game.service.GameLoop;
 
 public class ClosestEnemyDetector implements Detector {
     private final GameContext gameContext;
     private final int targetMask;
+    private final boolean useHorizontalDistance;
 
     public ClosestEnemyDetector(GameContext gameContext, int targetMask) {
+        this(gameContext, targetMask, false);
+    }
+
+    public ClosestEnemyDetector(GameContext gameContext, int targetMask, boolean useHorizontalDistance) {
         this.gameContext = gameContext;
         this.targetMask = targetMask;
+        this.useHorizontalDistance = useHorizontalDistance;
     }
 
     @Override
@@ -24,7 +29,9 @@ public class ClosestEnemyDetector implements Detector {
 
             if((TargetMask.of(target) & targetMask) == 0) continue;
 
-            double distance = self.getPosition().distance(target.getPosition());
+            double distance = useHorizontalDistance
+                    ? self.getPosition().toVector2().distance(target.getPosition().toVector2())
+                    : self.getPosition().distance(target.getPosition());
             if (distance < closestDistance) {
                 closestDistance = distance;
                 closest = target;
