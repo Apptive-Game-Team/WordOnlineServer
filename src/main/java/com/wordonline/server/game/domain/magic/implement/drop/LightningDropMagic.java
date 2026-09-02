d10e8c455b86635eeb52af73ade3c63e2d46a529
dev-yunseong <me@yunseong.dev>
2026-09-02 11:23:54 +0900

    fix(magic): spawn lightning drop at y 0
    Closes #476

--- src/main/java/com/wordonline/server/game/domain/magic/implement/drop/LightningDropMagic.java
+++ src/main/java/com/wordonline/server/game/domain/magic/implement/drop/LightningDropMagic.java
@@ -2,14 +2,12 @@ package com.wordonline.server.game.domain.magic.implement.drop;
 
 import org.springframework.stereotype.Component;
 
-import com.wordonline.server.game.config.GameConfig;
 import com.wordonline.server.game.domain.object.prefab.PrefabType;
 
 @Component("lightning_drop")
 public class LightningDropMagic extends AbstractDropMagic {
 
     public LightningDropMagic() {
-        super(PrefabType.LightningCloud, GameConfig.AERIAL_STANDARD_HEIGHT);
+        super(PrefabType.LightningCloud, 0f);
     }
 }
-
